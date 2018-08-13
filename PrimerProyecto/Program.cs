46761317994bc833a2b30d0f19b95290e87feb83
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-13 08:14:54 -0700

    Agregar archivos de proyecto.

---
+++ PrimerProyecto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona(25, "Jose");
+
+            Console.WriteLine(persona1.nombre + " Tiene " + persona1.edad + " años ");
+
+            Console.ReadLine();
+        }
+    }
+}
